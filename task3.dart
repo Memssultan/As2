483bd526c5c9a3ecc0d5e21ee5ac953f8490a2fe
Mems <150360316+Memssultan@users.noreply.github.com>
2023-11-18 15:29:30 +0600

    Update task3.dart

--- task3.dart
+++ task3.dart
@@ -5,20 +5,10 @@ void main() {
   String height = stdin.readLineSync()!;
   stdout.write('Enter the base:');
   String base = stdin.readLineSync()!;
-  int area1 = calculateArea1(int.parse(height));
-  int area2 = calculateArea2(int.parse(base));
-  int area3 = calculateArea3(int.parse(height));
-  print(area3);
+  double area3 = calculateArea3(double.parse(height), double.parse(base));
+  print('The area is: $area3');
 }
 
-int calculateArea1(int height) {
-  return height;
-}
-
-int calculateArea2(int base) {
-  return base;
-}
-
-int calculateArea3(height, base) {
-  return calculateArea1(height) * calculateArea2(base);
+double calculateArea3(double height, double base) {
+  return 0.5 * height * base;
 }
